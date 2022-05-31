f0559ad9f3dc0fecfc3b46f4f9cc82741a2edf2a
Joshua Negri <joshuanegri@icloud.com>
2022-05-31 12:27:30 +0200

    Specify test classes in SpringBootTest annotation

--- gestionequestionariSpring/src/test/java/it/f2informatica/questionari/GestionequestionariSpringApplicationTests.java
+++ gestionequestionariSpring/src/test/java/it/f2informatica/questionari/GestionequestionariSpringApplicationTests.java
@@ -1,13 +1,18 @@
 package it.f2informatica.questionari;
 
+import it.f2informatica.questionari.controller.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {DomandeControllerTest.class,
+        QuestionarioControllerTest.class,
+        QuestionarioUtenteControllerTest.class,
+        RisposteUtenteControllerTest.class,
+        UtenteControllerTest.class})
 class GestionequestionariSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
