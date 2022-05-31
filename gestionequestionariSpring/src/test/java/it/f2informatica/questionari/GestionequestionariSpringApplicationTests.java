e1f3db10a7236924903058955f867de3e5ef8446
Joshua Negri <joshuanegri@icloud.com>
2022-05-31 16:06:08 +0200

    Revert to working version

--- gestionequestionariSpring/src/test/java/it/f2informatica/questionari/GestionequestionariSpringApplicationTests.java
+++ gestionequestionariSpring/src/test/java/it/f2informatica/questionari/GestionequestionariSpringApplicationTests.java
@@ -1,18 +1,13 @@
 package it.f2informatica.questionari;
 
-import it.f2informatica.questionari.controller.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {DomandeControllerTest.class,
-        QuestionarioControllerTest.class,
-        QuestionarioUtenteControllerTest.class,
-        RisposteUtenteControllerTest.class,
-        UtenteControllerTest.class})
+@SpringBootTest
 class GestionequestionariSpringApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
